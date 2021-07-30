166864406c17aa4ed176551074bf085cb00ed6c2
rajib-sadhu <80105154+rajib-sadhu@users.noreply.github.com>
2021-07-31 02:10:22 +0530

    API graphics add

--- index-js.js
+++ index-js.js
@@ -1,9 +1,38 @@
 //pageloader
 
-    //alert("page loader check");
 
-let pageLoader=document.getElementById('page_loader');
+const pageLoader=document.getElementById('page_loader');
 
 function pageLoad(){
     pageLoader.style.display= 'none';
 }
+
+
+// Design Photos
+
+
+const centerId= document.getElementById('designId');
+
+
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=50").then((apiData) =>{ // "?_limit=20 for" limited data
+
+
+
+ return apiData.json();
+
+}).then((jsonData) =>{ 
+
+
+ jsonData.map((DesignVal) => {
+
+     key=DesignVal.id;
+     let Designimg = document.createElement("img");
+     Designimg.src=` https://picsum.photos/250/300?random=1${DesignVal.id} `;
+     Designimg.title='API Photos - Site Under Development';
+
+     centerId.appendChild(Designimg);
+
+
+ });
+
+});
